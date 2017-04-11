957334f2befac5dd8f43135e78e62bb5621653a0
Eric Dow <ericdow@alum.mit.edu>
2017-04-10 21:23:22 -0400

    finally got OpenGL stuff to link

--- src/opengl_test.cpp
+++ src/opengl_test.cpp
@@ -1,75 +1,41 @@
-// Include standard headers
-#include <stdio.h>
-#include <stdlib.h>
+#include <GL/glut.h>
 
-// Include GLEW
-#include <GL/glew.h>
 
-// Include GLFW
-#include <GLFW/glfw3.h>
-GLFWwindow* window;
+void draw(void) {
 
-// Include GLM
-#include <glm/glm.hpp>
-using namespace glm;
+    // Black background
+    glClearColor(0.0f,0.0f,0.0f,1.0f);
+    glClear(GL_COLOR_BUFFER_BIT);
+    //Draw i
+    glFlush();
 
-int main( void )
-{
-	// Initialise GLFW
-	if( !glfwInit() )
-	{
-		fprintf( stderr, "Failed to initialize GLFW\n" );
-		getchar();
-		return -1;
-	}
+}
 
-	glfwWindowHint(GLFW_SAMPLES, 4);
-	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
-	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
-	glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE); // To make MacOS happy; should not be needed
-	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+//Main program
 
-	// Open a window and create its OpenGL context
-	window = glfwCreateWindow( 1024, 768, "Tutorial 01", NULL, NULL);
-	if( window == NULL ){
-		fprintf( stderr, "Failed to open GLFW window. If you have an Intel GPU, they are not 3.3 compatible. Try the 2.1 version of the tutorials.\n" );
-		getchar();
-		glfwTerminate();
-		return -1;
-	}
-	glfwMakeContextCurrent(window);
+int main(int argc, char **argv) {
 
-	// Initialize GLEW
-	if (glewInit() != GLEW_OK) {
-		fprintf(stderr, "Failed to initialize GLEW\n");
-		getchar();
-		glfwTerminate();
-		return -1;
-	}
+    glutInit(&argc, argv);
 
-	// Ensure we can capture the escape key being pressed below
-	glfwSetInputMode(window, GLFW_STICKY_KEYS, GL_TRUE);
+    /*Setting up  The Display
+    /    -RGB color model + Alpha Channel = GLUT_RGBA
+    */
+    glutInitDisplayMode(GLUT_RGBA|GLUT_SINGLE);
 
-	// Dark blue background
-	glClearColor(0.0f, 0.0f, 0.4f, 0.0f);
+    //Configure Window Postion
+    glutInitWindowPosition(50, 25);
 
-	do{
-		// Clear the screen. It's not mentioned before Tutorial 02, but it can cause flickering, so it's there nonetheless.
-		glClear( GL_COLOR_BUFFER_BIT );
+    //Configure Window Size
+    glutInitWindowSize(480,480);
 
-		// Draw nothing, see you in tutorial 2 !
+    //Create Window
+    glutCreateWindow("Hello OpenGL");
 
-		
-		// Swap buffers
-		glfwSwapBuffers(window);
-		glfwPollEvents();
 
-	} // Check if the ESC key was pressed or the window was closed
-	while( glfwGetKey(window, GLFW_KEY_ESCAPE ) != GLFW_PRESS &&
-		   glfwWindowShouldClose(window) == 0 );
+    //Call to the drawing function
+    glutDisplayFunc(draw);
 
-	// Close OpenGL window and terminate GLFW
-	glfwTerminate();
-
-	return 0;
+    // Loop require by OpenGL
+    glutMainLoop();
+    return 0;
 }
