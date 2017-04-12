4236df4426baed87b1abbcbb9f7fa11d2a4caf2d
Eric Dow <ericdow@alum.mit.edu>
2017-04-12 19:02:39 -0400

    added joystick code

--- src/eigen_test.cpp
+++ src/eigen_test.cpp
@@ -2,6 +2,6 @@
 #include <Eigen/Dense>
 
 int main(int argc, char* argv[]) {
-  Eigen::Vector3d v(1,2,3), w(1,0,0);
+  Eigen::Matrix<float,3,1> v(1,2,3), w(1,0,0);
   std::cout << v.cross(w) << std::endl;
 }
