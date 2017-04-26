b41d3e183bd82d4d0078b90e7a92923f7d8d93a0
ericdow <ericdow@alum.mit.edu>
2017-04-25 21:46:57 -0400

    started working through shader tutorial

--- src/learnopengl/hello_triangle.cpp
+++ src/learnopengl/hello_triangle.cpp
@@ -8,7 +8,6 @@
 // GLFW
 #include <GLFW/glfw3.h>
 
-
 // Function prototypes
 void key_callback(GLFWwindow* window, int key, int scancode, int action, int mode);
 
@@ -16,19 +15,22 @@ void key_callback(GLFWwindow* window, int key, int scancode, int action, int mod
 const GLuint WIDTH = 800, HEIGHT = 600;
 
 // Shaders
-const GLchar* vertexShaderSource = "#version 330 core\n"
+const GLchar* vertexShaderSource = 
+    "#version 330 core\n"
     "layout (location = 0) in vec3 position;\n"
     "void main()\n"
     "{\n"
     "gl_Position = vec4(position.x, position.y, position.z, 1.0);\n"
     "}\0";
-const GLchar* orangeFragmentShaderSource = "#version 330 core\n"
+const GLchar* orangeFragmentShaderSource = 
+    "#version 330 core\n"
     "out vec4 color;\n"
     "void main()\n"
     "{\n"
     "color = vec4(1.0f, 0.5f, 0.2f, 1.0f);\n"
     "}\n\0";
-const GLchar* yellowFragmentShaderSource = "#version 330 core\n"
+const GLchar* yellowFragmentShaderSource = 
+    "#version 330 core\n"
     "out vec4 color;\n"
     "void main()\n"
     "{\n"
