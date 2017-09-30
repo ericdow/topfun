fbbc4d31c7f8091de94453b85e6888e39be9b633
Eric <ericdow@alum.mit.edu>
2017-09-30 12:28:16 -0400

    added cloud shadows, still needs performance optimization

--- src/shaders/clouds_raymarch.fs
+++ src/shaders/clouds_raymarch.fs
@@ -23,16 +23,22 @@ uniform float cloud_end;
 uniform float max_cloud_height; // maximum cloud vertical thickness
 
 // Sun parameters
-uniform vec3 sun_dir;
+uniform vec3 sun_dir; // normalized, points towards sun
 uniform vec3 sun_color;
 
-// Define some constants
+// Define some constants for phase functions
 const float one_over_four_pi = 1.0 / 4.0 / 3.14159265;
 const float g = 0.9;
 const float k_schlick = 1.5 * g - 0.55 * g * g * g;
 const float num_schlick = (1 - k_schlick*k_schlick) * one_over_four_pi;
 const float sigma_extinction = 1.0; // larger is thicker
 const float sigma_scattering = 0.95; // larger is brighter
+
+// Define some constants for shadows
+const int n_shadow_steps = 4; // step_size0_over_l must be consistent
+const float step_size_growth = 1.2;
+const float step_size0_over_l = 1.0 / (1.0 + step_size_growth + 
+  pow(step_size_growth, 2) + pow(step_size_growth, 3));
 
 // Parabolic density multiplier
 float GetHeightAttenuation(float h_frac) {
@@ -42,6 +48,14 @@ float GetHeightAttenuation(float h_frac) {
 float GetHeightGradient(float h_frac) {
   float delta = 0.6;
   return clamp((1.0 + delta) * h_frac - delta, 0.0, 1.0);
+}
+
+// Compute the cloud start height based on the height and altitude values
+// h - cloud height at this location
+// altitude - fraction in [0, 1] of maximum cloud altitude
+float GetCloudStartHeight(float h, float altitude) {
+  return cloud_start + 0.5 * (max_cloud_height - h) + 
+    (cloud_end - cloud_start - max_cloud_height) * altitude;
 }
 
 // Compute the cloud density at a particular xyz position
@@ -82,24 +96,41 @@ vec3 CalcAmbientColor(float h_frac) {
   return mix(vec3(0.4, 0.4, 0.45), vec3(1.0, 1.0, 1.0), h_frac);
 }
 
-// Compute the cloud start height based on the height and altitude values
-// h - cloud height at this location
-// altitude - fraction in [0, 1] of maximum cloud altitude
-float GetCloudStartHeight(float h, float altitude) {
-  return cloud_start + 0.5 * (max_cloud_height - h) + 
-    (cloud_end - cloud_start - max_cloud_height) * altitude;
+// Compute the sun color by accounting for extinction due to shadows
+float CalcSunExtinction(in vec3 position) {
+  // Get Ray from position to sun, and find intersection with atmosphere end
+  Ray ray_to_sun = Ray(position, sun_dir);
+  float l_ray_to_sun = CalcRayPlaneIntersection(ray_to_sun, cloud_end);
+  l_ray_to_sun *= 0.99; // shorten a bit to stay completely inside atmosphere
+  // March towards sun and compute extinction
+  float extinction = 1.0;
+  float step_size = step_size0_over_l * l_ray_to_sun;
+  for (int i = 0; i < n_shadow_steps; ++i) {
+    position += step_size;
+    vec4 w = texture(weather, position.xz * weather_scale);
+    float h = max_cloud_height * w.g;
+    float y0 = GetCloudStartHeight(h, w.b);
+    float h_frac = clamp((position.y - y0) / h, 0.0, 1.0);
+    float density = GetDensity(position, w, h_frac);
+    float scattering_coeff = sigma_scattering * density;
+    float extinction_coeff = max(0.0000001, sigma_extinction * density);
+    float step_extinction = exp(-extinction_coeff * step_size);
+    extinction *= step_extinction;
+    step_size *= step_size_growth;
+  }
+  return extinction;
 }
 
 // Perform ray-marching to compute color and extinction
 vec4 RayMarch(Ray ray, vec2 start_stop) {
-  float extinction = 1.0;
-  vec3 scattering = vec3(0.0, 0.0, 0.0);
-  
-  int n_steps = 50;
+  // Ray-march to compute scattering and extinction
+  int n_steps = 20;
   float step_size = (start_stop.y - start_stop.x) / n_steps;
   float offset = rand(gl_FragCoord.xy) * step_size;
   vec3 position = ray.origin + (start_stop.x + offset) * ray.dir;
   vec3 dposition = step_size * ray.dir;
+  float extinction = 1.0;
+  vec3 scattering = vec3(0.0, 0.0, 0.0);
   for (int i = 0; i < n_steps; ++i) {
     vec4 w = texture(weather, position.xz * weather_scale);
     float h = max_cloud_height * w.g;
@@ -115,9 +146,10 @@ vec4 RayMarch(Ray ray, vec2 start_stop) {
       break;
     
     vec3 ambient_color = CalcAmbientColor(h_frac);
-    // TODO modify sun_color to account for shadows...
+    float sun_extinction = CalcSunExtinction(position);
     vec3 step_scattering = scattering_coeff *  
-      (CalcSunPhaseFunction(ray.dir) * sun_color + ambient_color);
+      (CalcSunPhaseFunction(ray.dir) * sun_color * sun_extinction 
+      + ambient_color);
     scattering += extinction * 
       (step_scattering - step_scattering * step_extinction) / extinction_coeff;
     extinction *= step_extinction;
@@ -191,18 +223,17 @@ vec2 GetRayAtmosphereIntersection(Ray ray) {
 
 // Blend the color obtained by raymarching with the previous frame
 vec4 TemporalBlend(vec4 color_in, vec4 eye_pos) {
-  eye_pos *= gl_FragCoord.w; // perspective divide
-  vec4 p_cs = projview_prev * eye_pos;
-  vec2 st = 0.5 * (p_cs.xy + 1.0);
-  if (st.x > 1.0 || st.x < 0.0 || st.y > 1.0 || st.y < 0.0) {
+  // eye_pos *= gl_FragCoord.w; // perspective divide
+  vec4 q_cs = projview_prev * eye_pos;
+  vec2 q_uv = 0.5 * q_cs.xy / q_cs.w + 0.5;
+  if (q_uv.x > 1.0 || q_uv.x < 0.0 || q_uv.y > 1.0 || q_uv.y < 0.0) {
     return color_in;
   }
   else {
-    vec4 color_prev = texture(texture_prev, st);
+    vec4 color_prev = texture(texture_prev, q_uv);
     float alpha = 0.05;
     return alpha * color_in + (1.0 - alpha) * color_prev;
   }
-  // return vec4(vec3(0.5 * (p_cs.y + 1.0)), 0.0);
 }
 
 void main() {    
@@ -218,5 +249,6 @@ void main() {
   if (start_stop.x >= 0.0f)
     scatter_extinction = RayMarch(ray, start_stop);
 
-  color = TemporalBlend(scatter_extinction, eye_pos);
+  color = scatter_extinction;
+  // color = TemporalBlend(scatter_extinction, eye_pos);
 }
